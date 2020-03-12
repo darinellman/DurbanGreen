eafbaaa8abc0da00da7e61f1d9aba405fc320a30
Darin <drnellman@gmail.com>
2020-03-12 21:03:01 +0200

    first commit

---
+++ durbangreen/urls.py
@@ -0,0 +1,43 @@
+"""durbangreen URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+#from . import views
+
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    #path('contact_us/',views.contact_us, name='contact_us'),
+    #path('', views.HomePage.as_view(), name='home'),
+    #path('members/',include('members.urls')),
+    #path('products/',include('products.urls')),
+    #path('blog/',include('blog.urls')),
+    #path('albums/', include('albums.urls')),
+    #path('neighbors/', include('neighbors.urls')),
+    #path('news/',include('news.urls')),
+    #path('events/',include('events.urls')),
+    #path('education/',include('education.urls')),
+    #path('strains/',include('strains.urls'))
+    #path('blog/', include('blog.urls')),
+    #path('trinkets/', include('trinkettracker.urls')),
+    #path('baersmerch/', include('baersmerch.urls')),
+    #path('trinkettracker/', include('trinkettracker.urls')),
+] # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
